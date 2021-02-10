0212ed0740e033251ae6bf936d66c81dd03aacbe
Casey <Casey@vouch.co.uk>
2021-02-10 12:16:20 +0000

    update mdx plugin and md file

--- docs/change1.md
+++ docs/change1.md
@@ -1,3 +1,8 @@
+---
+date: "2021-10-02"
+title: "February Changes"
+---
+
 # Change 1
 
 We did some great changes in this release. To name a few:
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,25 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-mdx",
+    `gatsby-transformer-remark`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
         path: "./src/pages/",
+        ignore: ["**/.*", "**/.*.tsx"],
       },
       __key: "pages",
+    },
+    {
+      resolve: `gatsby-source-git`,
+      options: {
+        name: `vouch-change-log`,
+        remote: `https://github.com/Casey6624/vouch-change-log.git`,
+        branch: `master`,
+        // Only import the docs folder from a codebase.
+        patterns: `docs/**`,
+      },
     },
   ],
 };
