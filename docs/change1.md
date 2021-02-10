0b763e93f59ecb8e123a72489120f52e609810f2
Casey <Casey@vouch.co.uk>
2021-02-10 10:11:44 +0000

    inital commit w/ plugin setup

---
+++ docs/change1.md
@@ -0,0 +1,9 @@
+# Change 1
+
+We did some great changes in this release. To name a few:
+
+- Change 1
+- Change 2
+- Change 3
+
+See? So many changes
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const PageWrapper = styled.div``;
+// markup
+const IndexPage = () => {
+  return (
+    <PageWrapper>
+      <h1>Vouch</h1>
+      <h5>Change Log</h5>
+      <p>wooo</p>
+    </PageWrapper>
+  );
+};
+
+export default IndexPage;
